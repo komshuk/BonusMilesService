cc9e2e56a83ed8ad40c83a9a86d77a14903c22ef
komshuk <88716968+komshuk@users.noreply.github.com>
2021-09-21 08:31:30 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
-        int miles = service.calculate(price);
+        long miles = service.calculate(price);
         System.out.println(miles);
     }
 }
